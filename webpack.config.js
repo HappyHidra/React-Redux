03290b1e41536f82d48865caf4f4a31b9ec9f830
HappyHidra <okmmiha@mail.ru>
2020-08-25 13:41:59 +0300

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.jsx",
+    output: { //собранное приложение
+        path: path.resolve(__dirname, '.', 'dist'),
+        filename: 'index-bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/, //проверить все файлы .js в проекте
+            exclude: /node_modules/, // исключение, конвертация файлов в этой папке не требуется
+            use: 'babel-loader' //для конвертации JS-файла, написанного на ES6, в совместимый с браузером ES5
+        }, ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html" //путь до index.html
+        })
+    ],
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+    }
+};
